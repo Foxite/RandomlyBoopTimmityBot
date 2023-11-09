b2d3da1f9a30e98f0c0edd4ddfcea2c50d06c7a9
Dirk Kok <the@dirkkok.nl>
2023-11-09 22:45:52 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RandomlyPingFishermanBot
+You specify a user ID and a guild ID, and at random intervals between 2 and 24 hours, this bot will ping them in a random channel in that guild.
+
+## Docker deployment
+See dockerfile in source code folder. Envvars:
+- BOT_TOKEN
+- TARGET_ID
+- GUILD_ID
---
+++ RandomlyBoopTimmityBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+
+string botToken = Environment.GetEnvironmentVariable("BOT_TOKEN")!;
+ulong targetId = ulong.Parse(Environment.GetEnvironmentVariable("TARGET_ID")!);
+string message = Environment.GetEnvironmentVariable("MESSAGE_CONTENTS")!;
+
+var random = new Random();
+var discord = new DiscordClient(new DiscordConfiguration() {
+	Token = botToken
+});
+
+await discord.ConnectAsync();
+
+while (true) {
+	await Task.Delay(TimeSpan.FromHours(random.NextDouble() * 22 + 2));
+
+	// Find a guild where the target is and we can DM them
+	foreach (DiscordGuild guild in discord.Guilds.Values) {
+		DiscordMember? targetMember = await guild.GetMemberAsync(targetId);
+		if (targetMember != null) {
+			await targetMember.SendMessageAsync(message);
+		}
+	}
+}
